72f4c14d9cd98557486caf0879567e3d3c92cfac
Hamusuta0320 <2326955513@qq.com>
2023-09-15 16:52:25 +0000

    feat: add code folder

---
+++ code/hello-rust/target/debug/deps/hello_rust-0921c78cdd7cbfbd.d
@@ -0,0 +1,5 @@
+/workspace/code/hello-rust/target/debug/deps/hello_rust-0921c78cdd7cbfbd: src/main.rs
+
+/workspace/code/hello-rust/target/debug/deps/hello_rust-0921c78cdd7cbfbd.d: src/main.rs
+
+src/main.rs:
---
+++ code/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/workspace/code/hello-rust/target/debug/hello-rust: /workspace/code/hello-rust/src/main.rs
